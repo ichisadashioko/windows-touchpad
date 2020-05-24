ed95d7fdb43d75a70010f4c84093670cf71c1212
Ichisada Shioko <ichisadashioko@gmail.com>
2020-05-24 09:26:23 +0900

    init gui and RegisterRawInputDevices
    - add VS project
    - indent switch statement with clang-format
    - print debug infor to file

---
+++ touchpad/src/touchpad.h
@@ -0,0 +1,18 @@
+// Following guide: https://docs.microsoft.com/en-us/cpp/windows/walkthrough-creating-windows-desktop-applications-cpp?view=vs-2019
+#pragma once
+
+#define _CRT_SECURE_NO_WARNINGS
+#define DEBUG_FILE "debug-output.log"
+
+#include <Windows.h>
+#include <stdlib.h>
+#include <string.h>
+#include <tchar.h>
+
+// Function signatures
+
+void debugf(const char *fmt, ...);
+
+LRESULT CALLBACK WndProc(_In_ HWND hWnd, _In_ UINT message, _In_ WPARAM wParam, _In_ LPARAM lParam);
+
+int CALLBACK WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow);
