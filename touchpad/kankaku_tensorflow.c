b29fde8307125a44bce41a57e757b84c5d22bb8f
shioko <ichisadashioko@gmail.com>
2021-01-17 19:29:16 +0000

    run clang-format

--- touchpad/kankaku_tensorflow.c
+++ touchpad/kankaku_tensorflow.c
@@ -5,6 +5,7 @@
 #include <tensorflow/c/c_api.h>
 #pragma comment(lib, "tensorflow.lib")
 
-void kankaku_tensorflow_hello_world() {
+void kankaku_tensorflow_hello_world()
+{
   printf("Hello from TensorFlow C library version %s\n", TF_Version());
 }
