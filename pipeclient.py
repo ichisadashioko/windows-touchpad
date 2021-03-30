32ae1aa03a0469a136b8c3cb492ccd9cfedb83f8
shioko <ichisadashioko@gmail.com>
2021-03-30 14:42:33 +0000

    learning ctypes

--- pipeclient.py
+++ pipeclient.py
@@ -12,93 +12,73 @@ import win32pipe
 import pywintypes
 
 # graphics modules
-import tkinter as tk
-import numpy as np
-import PIL
-import PIL.Image
-import PIL.ImageTk
-import cv2
 import cairo
+
+# fmt: off
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
+# fmt: on
+
+GENERIC_READ = ctypes.c_ulong(0x80000000)
+OPEN_EXISTING = ctypes.c_int(3)
 
 global_last_update_clock = None
 global_last_update_clock_lock = threading.Lock()
-global_tk_photo_ref = None
-global_np_image = None
 global_np_rgb_image = None
 
 
-class UpdateCanvasThread(threading.Thread):
-    def __init__(
-        self,
-        clock,
-        delay: float,
-        cairo_surface: cairo.ImageSurface,
-        tk_root: tk.Tk,
-        tk_canvas: tk.Canvas,
-    ):
-        super().__init__()
-        self.clock = clock
-        self.delay = delay
-        self.cairo_surface = cairo_surface
-        self.tk_root = tk_root
-        self.tk_canvas = tk_canvas
+class KankakuWindow(Gtk.Window):
+    def __init__(self):
+        Gtk.Window.__init__(self, title='kankaku')
 
-    def run(self):
-        global global_last_update_clock, global_last_update_clock, global_tk_photo_ref, global_np_image, global_np_rgb_image
+        self.drawingarea = Gtk.DrawingArea()
+        self.drawingarea.connect('draw', self.render_strokes)
+        self.add(self.drawingarea)
+        self.strokes = []
+        self.current_stroke = None
+        self.line_width = 12
+        self.line_cap = cairo.LINE_CAP_ROUND
 
-        time.sleep(self.delay)
+    def render_strokes(self, da, ctx):
+        print(time.perf_counter(), 'render_strokes')
 
-        global_last_update_clock_lock.acquire()
-        if self.clock == global_last_update_clock:
-            # TODO update canvas
-            # print(self.clock, 'excuted')
-            start_ts = time.perf_counter()
-            with io.BytesIO() as fileobj:
-                self.cairo_surface.write_to_png(fileobj)
-                pil_image = PIL.Image.open(fileobj)
-                global_tk_photo_ref = PIL.ImageTk.PhotoImage(image=pil_image)
-                self.tk_canvas.delete(tk.ALL)
-                self.tk_canvas.create_image(
-                    0, 0,
-                    anchor='nw',
-                    image=global_tk_photo_ref,
-                )
+        for stroke in self.strokes:
+            num_contacts = len(stroke)
+            if num_contacts == 0:
+                continue
+            elif num_contacts == 1:
+                # TODO
+                pass
+            else:
+                ctx.set_source_rgba(0.0, 1.0, 0.0, 1.0)
+                ctx.set_line_width(self.line_width)
+                ctx.set_line_cap(self.line_cap)
 
-                global_np_image = np.array(pil_image)
-                # print(type(pil_image), type(global_np_image), global_np_image.dtype, global_np_image.shape)
-                global_np_rgb_image = global_np_image[:, :, :3]
-                # print(type(global_np_rgb_image))
-                # print(global_np_rgb_image.shape, global_np_rgb_image.dtype)
+                is_first_point = True
+                for contact_info in stroke:
+                    x = contact_info['x']
+                    y = contact_info['y']
 
-            # TODO update tkinter window
-            # self.tk_root.update()
-            end_ts = time.perf_counter()
-            taken_time = end_ts - start_ts
-            print(f'took {taken_time} to render')
+                    if is_first_point:
+                        ctx.move_to(x, y)
+                        is_first_point = False
+                    else:
+                        ctx.line_to(x, y)
 
-        global_last_update_clock_lock.release()
+                ctx.stroke()
 
 
 class PipeClientThread(threading.Thread):
     def __init__(
         self,
-        tk_root,
-        tk_canvas,
-        render_thread_delay=.0078125,
+        kankaku_window: KankakuWindow,
     ):
         super().__init__()
 
-        self.tk_root = tk_root
-        self.tk_canvas = tk_canvas
-
         self.tracking_id = None
         self.last_contacts = {}
-        self.current_stroke = None
-        self.strokes = []
-        self.update_canvas = False
-        self.cairo_surface: cairo.ImageSurface = None
-        self.cairo_ctx: cairo.Context = None
-        self.render_thread_delay = render_thread_delay
+        self.kankaku_window = kankaku_window
 
     def translate_and_trigger_events(self, contact_info: dict):
         # translate to touch events
@@ -129,71 +109,53 @@ class PipeClientThread(threading.Thread):
     def on_touch_down(self, contact_info: dict):
         if self.tracking_id is None:
             self.tracking_id = contact_info['id']
-            self.current_stroke = [contact_info]
-
-            self.strokes.append(self.current_stroke)
-
-            # TODO update canvas
-            self.update_canvas = True
-
-            self.cairo_ctx.set_source_rgba(1.0, 1.0, 1.0, 1.0)
-            self.cairo_ctx.set_line_width(12)
-            self.cairo_ctx.set_line_cap(cairo.LINE_CAP_ROUND)
-            self.cairo_ctx.move_to(contact_info['x'], contact_info['y'])
-            self.cairo_ctx.stroke()
+            self.kankaku_window.current_stroke = [contact_info]
+            self.kankaku_window.strokes.append(self.kankaku_window.current_stroke)
 
     def on_touch_move(self, contact_info: dict):
         if self.tracking_id is not None:
             if self.tracking_id == contact_info['id']:
-                last_contact = self.current_stroke[-1]
+                last_contact = self.kankaku_window.current_stroke[-1]
 
                 if (last_contact['x'] == contact_info['x']) and (last_contact['y'] == contact_info['y']):
                     pass
                 else:
-                    self.current_stroke.append(contact_info)
-
-                    # TODO update canvas
-                    self.update_canvas = True
-
-                    self.cairo_ctx.set_source_rgba(1.0, 1.0, 1.0, 1.0)
-                    self.cairo_ctx.set_line_width(12)
-                    self.cairo_ctx.set_line_cap(cairo.LINE_CAP_ROUND)
-                    self.cairo_ctx.move_to(last_contact['x'], last_contact['y'])
-                    self.cairo_ctx.line_to(contact_info['x'], contact_info['y'])
-                    self.cairo_ctx.stroke()
+                    self.kankaku_window.current_stroke.append(contact_info)
 
     def on_touch_up(self, contact_info: dict):
         if self.tracking_id is not None:
             if self.tracking_id == contact_info['id']:
                 self.tracking_id = None
-                last_contact = self.current_stroke[-1]
+                last_contact = self.kankaku_window.current_stroke[-1]
 
                 if (last_contact['x'] == contact_info['x']) and (last_contact['y'] == contact_info['y']):
                     pass
                 else:
-                    self.current_stroke.append(contact_info)
+                    self.kankaku_window.current_stroke.append(contact_info)
 
-                    # TODO update canvas
-                    self.update_canvas = True
-
-                    self.cairo_ctx.set_source_rgba(1.0, 1.0, 1.0, 1.0)
-                    self.cairo_ctx.set_line_width(12)
-                    self.cairo_ctx.set_line_cap(cairo.LINE_CAP_ROUND)
-                    self.cairo_ctx.move_to(last_contact['x'], last_contact['y'])
-                    self.cairo_ctx.line_to(contact_info['x'], contact_info['y'])
-                    self.cairo_ctx.stroke()
-
-                self.current_stroke = None
+                self.kankaku_window.current_stroke = None
 
     def run(self):
         global global_last_update_clock, global_last_update_clock
 
         pipe_name = '\\\\.\\pipe\\kankaku'
+        pipe_name_bytes = pipe_name.encode('ASCII')
+        pipe_name_ptr = ctypes.create_string_buffer(pipe_name_bytes)
         print(pipe_name)
 
         READ_BUFFER_SIZE = 1024
 
         try:
+            ctypes.windll.kernel32.CreateFileA(
+                pipe_name_ptr,  # LPCSTR lpFileName
+                GENERIC_READ,  # DWORD dwDesiredAccess,
+                ctypes.c_ulong(0),  # DWORD dwShareMode
+                None,  # LPSECURITY_ATTRIBUTES lpSecurityAttributes
+                OPEN_EXISTING,  # DWORD dwCreationDisposition
+                ctypes.c_ulong(0),  # DWORD dwFlagsAndAttributes
+                None,  # HANDLE hTemplateFile
+            )
+
             handle = win32file.CreateFile(
                 pipe_name,
                 win32file.GENERIC_READ,
@@ -222,21 +184,9 @@ class PipeClientThread(threading.Thread):
             # TODO dynamically resize the rendered canvas while keeping the aspect ratio
             print(device_width, device_height)
 
-            # create a canvas with those dimensions
-            self.cairo_surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, device_width, device_height)
-
-            self.cairo_ctx = cairo.Context(self.cairo_surface)
-            self.cairo_ctx.set_source_rgba(1.0, 1.0, 1.0, 1.0)
-            self.cairo_ctx.set_line_width(12)
-            self.cairo_ctx.set_line_cap(cairo.LINE_CAP_ROUND)
+            # TODO resize Gtk window
 
             # TODO allow user to change stroke width
-
-            # resize the tkinter Canvas
-            self.tk_canvas.configure(
-                width=device_width,
-                height=device_height,
-            )
 
             remain_bs: bytes = res_bs[4:]
 
@@ -268,13 +218,6 @@ class PipeClientThread(threading.Thread):
 
                     global_last_update_clock = time.perf_counter()
                     # print(global_last_update_clock, 'started')
-                    UpdateCanvasThread(
-                        clock=global_last_update_clock,
-                        delay=self.render_thread_delay,
-                        cairo_surface=self.cairo_surface,
-                        tk_root=self.tk_root,
-                        tk_canvas=self.tk_canvas,
-                    ).start()
 
                     global_last_update_clock_lock.release()
 
@@ -286,47 +229,33 @@ class PipeClientThread(threading.Thread):
             print(type(win32error))
             print(win32error)
 
-        # TODO handling application life cycle
-        # self.tk_root.destroy()
+
+# def cv2_show_image_thread_fn():
+#     global global_np_rgb_image
+#     while True:
+#         if global_np_rgb_image is not None:
+#             cv2.imshow('frame', global_np_rgb_image)
+#             # print(np.max(global_np_rgb_image))
+#             k = cv2.waitKey(100)
+#             k = k & 0xff
+
+#             if k == ord('q') or k == 27:
+#                 break
+#         else:
+#             time.sleep(1)
+
+#     cv2.destroyAllWindows()
 
 
-np_canvas = None
-pil_canvas = None
-tk_canvas = None
-cairo_surface = None
+kankaku_window = KankakuWindow()
 
-tk_root = tk.Tk()
-tk_root.wm_title('kankaku client')
-tk_root.wm_attributes('-alpha', 0.7)
+# threading.Thread(target=cv2_show_image_thread_fn).start()
 
-tk_canvas = tk.Canvas(tk_root, width=640, height=480)
-tk_canvas.grid()
-
-
-def cv2_show_image_thread_fn():
-    global global_np_rgb_image
-    while True:
-        if global_np_rgb_image is not None:
-            cv2.imshow('frame', global_np_rgb_image)
-            # print(np.max(global_np_rgb_image))
-            k = cv2.waitKey(100)
-            k = k & 0xff
-
-            if k == ord('q') or k == 27:
-                break
-        else:
-            time.sleep(1)
-
-    cv2.destroyAllWindows()
-
-
-threading.Thread(target=cv2_show_image_thread_fn).start()
-
-pipe_client_thread = PipeClientThread(
-    tk_root=tk_root,
-    tk_canvas=tk_canvas,
-)
-
+pipe_client_thread = PipeClientThread(kankaku_window=kankaku_window)
 pipe_client_thread.start()
 
-tk_root.mainloop()
+kankaku_window.connect('destroy', Gtk.main_quit)
+kankaku_window.show_all()
+Gtk.main()
+
+pipe_client_thread.join()
